e3200fecaa62a00a3c01f5829ad2e2541c6ac641
Niharika655 <73527574+Niharika655@users.noreply.github.com>
2021-02-10 17:43:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,15 @@
-var students=[];
-function submit(){
-    var displayStudents=[];
-    for (var j = 1; j<=4; j++) {
-        var studentName=document.getElementById("name_of_the_student_"+j).value;
-        console.log(studentName);
-        students.push(studentName);
+function getParagraph1(){
+    var inputs = [];
+    for(var i = 1 ; i <=6 ; i++){
+        inputs.push(document.getElementById("para1_input" + i).value);
     }
-    console.log(students);
-    var length=students.length;
-    console.log(length);
-    for (var k = 0; k<length; k++) {
-     displayStudents.push("<h4>Name - "+students[k]+"</h4>");
-        
-    }
-    console.log(displayStudents); 
-    document.getElementById("display_name_with_commas").innerHTML=displayStudents;
-    var removecommas=displayStudents.join(" ");
-    console.log(removecommas);
-    document.getElementById("display_name_without_commas").innerHTML=removecommas;
-    document.getElementById("submit_button").style.display="none";
-    document.getElementById("sort_button").style.display="inline-block";
+    document.getElementById("showParagraph1").innerHTML = inputs.join(". ");
 }
-function sorting(){
-    students.sort();
-    var displayStudentsSort=[];
-    console.log(students);
-    var length=students.length;
-    console.log(length);
-    for (var k = 0; k<length; k++) {
-     displayStudentsSort.push("<h4>Name - "+students[k]+"</h4>");
-        
+
+function getParagraph2(){
+    var inputs1 = [];
+    for(var j = 1 ; j<=6 ; j++){
+        inputs1.push(document.getElementById("para2_input" + j).value);
     }
-    console.log(displayStudentsSort); 
-    var removecommas=displayStudentsSort.join(" ");
-    console.log(removecommas);
-    document.getElementById("display_name_without_commas").innerHTML=removecommas;
-   }
+      document.getElementById("showParagraph2").innerHTML = inputs1.join(". ");
+}
